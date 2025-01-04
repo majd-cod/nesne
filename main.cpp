47dd0b5f6509e71df36c39e2536e4038236bcaf3
majd-cod <b210109571@subu.edu.tr>
2025-01-04 14:39:27 +0300

    add gotoxy function for cursor position management in console

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ using namespace std;
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD CursorPosition;
 
+void gotoxy(int x, int y) {
+    CursorPosition.X = x;
+    CursorPosition.Y = y;
+    SetConsoleCursorPosition(console, CursorPosition);
+}
 
 
 
